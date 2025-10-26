fcacb02664be511c33b02814b90391c0e631062c
Chrysr17 <chrisrasaro@gmail.com>
2025-10-25 19:14:41 -0500

    UsuarioRepository

---
+++ src/main/java/com/example/authservice/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.example.authservice.repository;
+
+import com.example.authservice.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByNombreUsuario(String nombreUsuario);
+    Optional<Usuario> findByDni(String dni);
+}
