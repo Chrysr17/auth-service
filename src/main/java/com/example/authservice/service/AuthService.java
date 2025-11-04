ea4c862ceca714319f67946f41c19075b44f7acf
Chrysr17 <chrisrasaro@gmail.com>
2025-11-04 11:00:25 -0500

    AuthException

--- src/main/java/com/example/authservice/service/AuthService.java
+++ src/main/java/com/example/authservice/service/AuthService.java
@@ -3,9 +3,13 @@ package com.example.authservice.service;
 import com.example.authservice.dto.JwtResponse;
 import com.example.authservice.dto.LoginRequest;
 import com.example.authservice.dto.RegisterRequest;
+import com.example.authservice.entity.Rol;
 import com.example.authservice.entity.Usuario;
+import com.example.authservice.exception.AuthException;
 import com.example.authservice.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,32 +20,39 @@ public class AuthService {
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public JwtResponse login(LoginRequest request) {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getUsername(),
+                            request.getPassword()
+                    )
+            );
+
         Usuario usuario = usuarioRepository.findByUsername(request.getUsername())
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-
-        if (!passwordEncoder.matches(request.getPassword(), usuario.getPassword())) {
-            throw new RuntimeException("Contraseña incorrecta");
-        }
-
+                .orElseThrow(() -> new AuthException("Usuario no encontrado"));
         String token = jwtService.generateToken(usuario.getUsername(), usuario.getRol().name());
         return new JwtResponse(token);
     }
 
     public JwtResponse register(RegisterRequest request) {
-        if (usuarioRepository.findByUsername(request.getUsername()).isPresent()) {
-            throw new RuntimeException("El usuario ya existe");
+        if (usuarioRepository.existByUsername(request.getUsername())) {
+            throw new AuthException("El nombre de usuario ya está en uso");
+        }
+        if (usuarioRepository.existByEmail(request.getEmail())){
+            throw new AuthException("El correo ya está registrado");
         }
 
         Usuario usuario = Usuario.builder()
                 .username(request.getUsername())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .email(request.getEmail())
-                .rol(request.getRol())
+                .rol(request.getRol() == null ? Rol.USER : request.getRol())
                 .build();
 
         usuarioRepository.save(usuario);
+
         String token = jwtService.generateToken(usuario.getUsername(), usuario.getRol().name());
         return new JwtResponse(token);
     }
