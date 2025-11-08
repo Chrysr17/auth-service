bbcc00bb3ff346890769ed867e7e9042e72fca3d
Chrysr17 <chrisrasaro@gmail.com>
2025-11-08 15:12:49 -0500

    GlobalException

--- src/main/java/com/example/authservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/authservice/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.password.CompromisedPasswordException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
